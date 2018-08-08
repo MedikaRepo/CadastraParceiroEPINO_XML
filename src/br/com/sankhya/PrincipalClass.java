375513f67ef4eb04299b08ba42866b61c5c53be1
STI-004 <STI-004@DESKTOP-AR7UDI6>
2018-08-08 16:25:45 -0300

    Insercao de novos campos e correcao de telefone

--- src/br/com/sankhya/PrincipalClass.java
+++ src/br/com/sankhya/PrincipalClass.java
@@ -41,7 +41,7 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 		}
 		catch(SQLException ex)
 		{
-			//System.err.println("SQLException: " + ex.getMessage());
+			System.err.println("SQLException: " + ex.getMessage());
 			return null;
 		}
 	}	 
@@ -143,7 +143,7 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 					contLinhas++;
 				}
 
-				if (temp.contains("<![CDATA[_billing_cellphone]]>")){
+				if (temp.contains("<![CDATA[_billing_phone]]>")){
 					linha[contLinhas] = buffRead.readLine();
 					linha[contLinhas] = linha[contLinhas].substring(27, linha[contLinhas].length());
 					linha[contLinhas] = linha[contLinhas].substring(0, linha[contLinhas].length()-19);
@@ -202,6 +202,10 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 		int codEnd = 0;
 		int ultCodBai = 0;
 		int codBai = 0;
+		int codReg = 0;
+		short sUltCodUf = 0;
+		short sCodUf = 0;
+		String sexo = "F";
 		try {
 
 			//Conecta no banco do Sankhya
@@ -218,10 +222,20 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 			}
 
 			mensagem.append("Parceiros cadastrados: \n");
+			//for(int i=0;i<14; i++)
+			//{
+			//	System.out.println("Posicao "+i+"="+linha[i]);
+			//}
 			for (int i = 0; i < linha.length; i=i+14) {
-				String cpf = linha[i+8].replace( "." , ""); //tira ponto
+				
+				if (linha[0].substring(linha[0].length()-1, linha[0].length())=="o") {
+					sexo = "M";
+				}
+				
+				String cpf = linha[i+9].replace( "." , ""); //tira ponto
 				cpf = cpf.replace("-", "");
 				cpf = cpf.replace(" ", "");
+				//System.out.println(cpf);
 				if (ExecutaComandoNoBanco("SELECT CGC_CPF FROM TGFPAR WHERE CGC_CPF='"+cpf+"'", "select")==null)
 				{
 					//ultimo parceiro
@@ -235,18 +249,27 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 						ultCodEnd = (Integer)ExecutaComandoNoBanco("SELECT max(codend) FROM tsiend", "select");
 					}
 
+					if(ExecutaComandoNoBanco("SELECT coduf FROM tsiufs where uf = '"+linha[i+6]+"'", "select")!=null) {
 
-					if(ExecutaComandoNoBanco("SELECT codbai FROM tsibai where nomebai = '"+linha[i+10]+"'", "select")!=null) {
+						sCodUf = (Short)ExecutaComandoNoBanco("SELECT coduf FROM tsiufs where uf = '"+linha[i+6]+"'", "select");
+					}else {
+						//ultimo endereco
+						sUltCodUf = (Short)ExecutaComandoNoBanco("SELECT max(coduf) FROM tsiufs", "select");
+					}
 
-						codBai = (Integer)ExecutaComandoNoBanco("SELECT codbai FROM tsibai where nomebai = '"+linha[i+10]+"'", "select");
+					//System.out.println("SELECT codbai FROM tsibai where nomebai = '"+linha[i+11]+"'");
+					if(ExecutaComandoNoBanco("SELECT codbai FROM tsibai where nomebai = convert(varchar(20),'"+linha[i+11]+"')", "select")!=null) {
+
+						codBai = (Integer)ExecutaComandoNoBanco("SELECT codbai FROM tsibai where nomebai = convert(varchar(20),'"+linha[i+11]+"')", "select");
 					}else {
 						//ultimo bairro
 						ultCodBai = (Integer)ExecutaComandoNoBanco("SELECT max(codbai) FROM tsibai", "select");
 					}
 
-					if(ExecutaComandoNoBanco("SELECT codcid FROM tsicid where nomecid like '%"+linha[i+4]+"%'", "select")!=null) {
+					//System.out.println("SELECT codcid FROM tsicid where nomecid like '%'+convert(varchar(20),'"+linha[i+5]+"')+'%'");
+					if(ExecutaComandoNoBanco("SELECT codcid FROM tsicid where nomecid like '%'+convert(varchar(20),'"+linha[i+5]+"')+'%'", "select")!=null) {
 
-						codCid = (Integer)ExecutaComandoNoBanco("SELECT codcid FROM tsicid where nomecid like '%"+linha[i+4]+"%'", "select");
+						codCid = (Integer)ExecutaComandoNoBanco("SELECT codcid FROM tsicid where nomecid like '%'+convert(varchar(20),'"+linha[i+5]+"')+'%'", "select");
 					}else {
 						//ultima cidade
 						ultCodCid = (Integer)ExecutaComandoNoBanco("SELECT max(codcid) FROM tsicid", "select");
@@ -261,24 +284,47 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 
 					if(codBai == 0) {
 						//insere  bairro
+						//System.out.println("insert into tsibai (codbai, nomebai, codreg, dtalter)"
+							//	+ "values ("+(ultCodBai+1)+", '"+linha[i+11]+"', 0, getdate() )");
 						ExecutaComandoNoBanco("insert into tsibai (codbai, nomebai, codreg, dtalter)"
-								+ "values ("+(ultCodBai+1)+", '"+linha[i+10]+"', 0, getdate() )", "alter");
+								+ "values ("+(ultCodBai+1)+", convert(varchar(20),'"+linha[i+11]+"'), 0, getdate() )", "alter");
 						codBai = ultCodBai;
 					}
 
+					
+					//insere cidade
+					if (sCodUf == 0){
+						sUltCodUf++;
+						//System.out.println("insert into tsiufs (coduf, uf, descricao, valsitcadsefaz, estornonfe)"
+						//		+ "values ("+(sUltCodUf)+", '"+linha[i+6]+"', '"+linha[i+6]+"','N', 'N')");
+						ExecutaComandoNoBanco("insert into tsiufs (coduf, uf, descricao, valsitcadsefaz, estornonfe)"
+								+ "values ("+(sUltCodUf)+", '"+linha[i+6]+"', '"+linha[i+6]+"','N', 'N')", "alter");
+						sCodUf = sUltCodUf;
+					}
+					
 					//insere cidade
 					if (codCid == 0){
+						ultCodCid++;
+						//System.out.println("insert into tsicid (codcid, nomecid, uf, codreg, dtalter, qtddiassub, qtdsub,"
+							//	+ "TEMSUBSTITNFSE, TIPCANCNFSE)"
+								//+ "values ("+(ultCodCid)+", '"+linha[i+5]+"', '"+sCodUf+"',0, getdate(), 0, 0, 'N', 1)");
 						ExecutaComandoNoBanco("insert into tsicid (codcid, nomecid, uf, codreg, dtalter, qtddiassub, qtdsub,"
 								+ "TEMSUBSTITNFSE, TIPCANCNFSE)"
-								+ "values ("+(ultCodCid+1)+", '"+linha[i+12]+"', '"+linha[i+5]+"',0, getdate(), 0, 0, 'N', 1)", "alter");
-						codCid = ultCodCid+1;
+								+ "values ("+(ultCodCid)+", convert(varchar(20),'"+linha[i+5]+"'), '"+sCodUf+"',0, getdate(), 0, 0, 'N', 1)", "alter");
+						codCid = ultCodCid;
+					}
+					
+					
+					if(ExecutaComandoNoBanco("SELECT codreg FROM ad_tsirci where codcid="+codCid, "select")!=null) {
+
+						codReg = (Integer)ExecutaComandoNoBanco("SELECT codreg FROM ad_tsirci where codcid="+codCid, "select");
 					}
 
-					String cep = linha[i+6].replace( "." , ""); //tira ponto
+					String cep = linha[i+7].replace( "." , ""); //tira ponto
 					cep = cep.replace("-", "");
 					cep = cep.replace(" ", "");
 
-					String telefone = linha[i+11].replace( "." , ""); //tira ponto
+					String telefone = linha[i+4].replace( "." , ""); //tira ponto
 					telefone = telefone.replace("-", "");
 					telefone = telefone.replace(" ", "");
 					telefone = telefone.replace("(", "");
@@ -301,19 +347,21 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 							+ "'I', 'I', 'A', 'A', 'N', 'N', 0, 0, 'B', '"+linha[i+12]+"', "+codCid+", "+codEnd+", "+codBai+")");*/
 
 					//insere na tgfpar
-					ExecutaComandoNoBanco("insert into tgfpar (codparc, codvend, nomeparc, tippessoa, cep, telefone, email, dtcad, "
-							+ "dtalter, cgc_cpf, cliente, fornecedor, transportadora, vendedor, agencia, codtipparc, codbco, tare, IPIINCICMS, "
-							+ "retemiss, reteminss, ativo, bloquear, calcinss, codusu, retempis, retemcofins, retemcsl, usuario, temipi,"
-							+ "aluno, professor, descbonif, implaudolote, medico, tipofatur, motorista, descstiva, operlogist, agronomo, "
-							+ "produtortemnf, flex, simples, tipanexonfe, codusucobr, emaildanfe, permitecorte, statusedz, sitreceita, "
-							+ "sitsintegra, sitccf, usatabcrforn, artprecad, GERCTRCAUTO, codlocalpadrao, entregaendcontato, retstvenda, "
-							+ "tipogerboleto, TIPOGERBOLETODUP, BOLDESTEMAIL, ENVIPEDEMAILTOP, APLICLEITRANSP, integraeconect, baseparceleconect,"
-							+ "baseprazoeconect, perfileconect, emailnfe, codcid, codend, codbai)"
-							+ "values ("+(ultCodParc)+",0, '"+linha[i+13]+"', 'F',  '"+cep+"','"+telefone+"','"+linha[i+12]+"', getdate(), "
-							+ "getdate(), '"+cpf+"', 'S', 'N', 'N', 'N', 'N', 10301000, 0, 'N', 'S', 'N', 'N', 'S', 'N', "
-							+ "'N', 0, 'N', 'N', 'N', 'N', 'S', 'N', 'N', 'L', 'N', 'N', 'L', 'N', 'S', 'N', 'N', 'S',"
-							+ "'S', 'N', 'X', 0, 'S', 'S', 'P', 'I', 'I', 'I', 'N', 'N', 'N', 0, 'N', 'N',"
-							+ "'I', 'I', 'A', 'A', 'N', 'N', 0, 0, 'B', '"+linha[i+12]+"', "+codCid+", "+codEnd+", "+codBai+")", "alter");	
+					ExecutaComandoNoBanco("insert into tgfpar (codparc, codreg, codvend, nomeparc, razaosocial,tippessoa, cep, telefone, email, dtcad, "
+						+ "dtalter, cgc_cpf, cliente, fornecedor, transportadora, vendedor, agencia, codtipparc, codbco, tare, IPIINCICMS, "
+						+ "retemiss, reteminss, ativo, bloquear, calcinss, codusu, retempis, retemcofins, retemcsl, usuario, temipi,"
+						+ "aluno, professor, descbonif, implaudolote, medico, tipofatur, motorista, descstiva, operlogist, agronomo, "
+						+ "produtortemnf, flex, simples, tipanexonfe, codusucobr, emaildanfe, permitecorte, statusedz, sitreceita, "
+						+ "sitsintegra, sitccf, usatabcrforn, artprecad, GERCTRCAUTO, codlocalpadrao, entregaendcontato, retstvenda, "
+						+ "tipogerboleto, TIPOGERBOLETODUP, BOLDESTEMAIL, ENVIPEDEMAILTOP, APLICLEITRANSP, integraeconect, baseparceleconect,"
+						+ "baseprazoeconect, perfileconect, emailnfe, codcid, codend, codbai, codemppref, CODPARCMATRIZ, IDENTINSCESTAD, sexo,"
+						+ "numend, complemento, ad_tipcliente)"
+						+ "values ("+(ultCodParc)+","+codReg+",0, '"+linha[i+13]+"', '"+linha[i+13]+"', 'F',  '"+cep+"','"+telefone+"','"+linha[i+12]+"', getdate(), "
+						+ "getdate(), '"+cpf+"', 'S', 'N', 'N', 'N', 'N', 10301000, 0, 'N', 'S', 'N', 'N', 'S', 'N', "
+						+ "'N', 0, 'N', 'N', 'N', 'N', 'S', 'N', 'N', 'L', 'N', 'N', 'L', 'N', 'S', 'N', 'N', 'S',"
+						+ "'S', 'N', 'X', 0, 'S', 'S', 'P', 'I', 'I', 'I', 'N', 'N', 'N', 0, 'N', 'N',"
+						+ "'I', 'I', 'A', 'A', 'N', 'N', 0, 0, 'B', '"+linha[i+12]+"', "+codCid+", "+codEnd+", "+codBai+",3,0, 'ISENTO',"
+						+ "'"+sexo+"', '"+linha[10]+"','"+linha[3]+"',1 )", "alter");	
 
 					//insere na tgfctt
 					ExecutaComandoNoBanco("insert into tgfctt (codparc, codcontato, nomecontato, telefone, email, codparccad, possuiacessobt,"
@@ -332,11 +380,14 @@ public class PrincipalClass {//implements AcaoRotinaJava {   //implements Schedu
 							+ "values ("+(ultCodParc)+",0, 30902001, 0, getdate(), getdate(), 0,  0, getdate() )", "alter");
 
 					mensagem.append(ultCodParc+" - "+linha[i+13]+" CPF:"+cpf+" Email: "+linha[i+12]+" Telefone:"+telefone+" - Endereco: "+linha[i+2]+" "+linha[i+9]+" Complemento: "+linha[i+3]+" Bairro: "+linha[i+10]+" - "+linha[i+4]+" - "+linha[i+5]+" - "+linha[i+7]+" CEP: "+cep+"\n");
+				    if (codReg == 0) {
+				    	mensagem.append("\nE necessario definir a regiao do parceiro: "+ultCodParc+"\n");
+				    }
 				}
 			}
 		}catch (Exception e) {
         	//contexto.setMensagemRetorno(mensagem.toString()+e.getMessage());
-			//System.out.println(mensagem.toString()+e.getMessage());
+			System.out.println(e.getMessage());
         }
 		//contexto.setMensagemRetorno(mensagem.toString());
 		System.out.println(mensagem.toString());
